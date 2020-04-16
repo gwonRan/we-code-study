0901d286fe9d3e097eeb4c59941646265245f969
gwonmiran <gwon.rran@gmail.com>
2020-04-17 01:08:27 +0900

    python

---
+++ python/python_inflearn/fahrenheit/python_fahrenheit.py
@@ -0,0 +1,11 @@
+print("본 프로그램은 섭씨를 화씨로 바꿔주는 프로그램 입니다.")
+print("변환하고 싶은 섭씨온도를 입력해주세요.")
+
+user_input = float(input())
+b = ((9/5)* user_input) + 32
+
+print("섭씨온도 : ", user_input)
+print("화씨온도 :", b)
+
+
+
